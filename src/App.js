05b238c9e229ad5dcd9a624c923a2355d43fce06
Miraç Doğan <miracdogn@gmail.com>
2023-05-30 22:09:28 +0300

    Form Inputs Added to the page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 const App = () => {
@@ -30,6 +33,7 @@ const App = () => {
 
   return (
     <div>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
